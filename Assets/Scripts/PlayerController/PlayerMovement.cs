b73f0cd193640363d2f98239fd983bdd4588b57f
WCAF0137C <60712316+WCAF0137C@users.noreply.github.com>
2026-01-14 02:23:09 -0500

    Camera and Controller beginnings

---
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 0.04f;
+    public float strafeSpeed = 0.04f;
+
+    public float horizontal;
+    public float vertical;
+
+    public Vector3 move;
+
+    public CharacterController playerController;
+
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        move = new Vector3(horizontal, 0, vertical);
+        playerController.Move(move * Time.deltaTime * moveSpeed);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerController.Move(transform.TransformDirection(Vector3.forward) * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerController.Move(transform.TransformDirection(Vector3.back) * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerController.Move(transform.TransformDirection(Vector3.left) * strafeSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerController.Move(transform.TransformDirection(Vector3.right) * strafeSpeed);
+        }
+    }
+}
