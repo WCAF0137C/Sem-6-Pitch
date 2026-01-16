884a693b1ef13c3e1ab9ed8adf93317377d433ea
WCAF0137C <60712316+WCAF0137C@users.noreply.github.com>
2026-01-15 23:26:51 -0500

    Update EvidenceFolder.cs

--- Assets/Scripts/Evidence Folder/EvidenceFolder.cs
+++ Assets/Scripts/Evidence Folder/EvidenceFolder.cs
@@ -3,12 +3,30 @@ using UnityEngine.UI;
 
 public class EvidenceFolder : MonoBehaviour
 {
-    //This script functions as a menu system for our evidence folder
+    public enum CluesEnum
+    {
+        Overview,
+        Details
+    }
 
+    public enum InfoEnum
+    {
+        Report,
+        Questions,
+        Inventory
+    }
+
+    //This script functions as a menu system for our evidence folder
+    public GameObject folderCanvas; // Animate later
+
+    // Clues section
     public GameObject clueOverviewPanel;
     public GameObject clueDetailsPanel;
     public Button clueReturnButton;
 
+    public CluesEnum currentCluePanel = CluesEnum.Overview;
+
+    // Info section
     public GameObject reportPanel;
     public GameObject questionsPanel;
     public GameObject inventoryPanel;
@@ -17,9 +35,14 @@ public class EvidenceFolder : MonoBehaviour
     public Button questionsButton;
     public Button inventoryButton;
 
+    public InfoEnum currentInfoPanel = InfoEnum.Report;
+
     void Start()
     {
-        
+        questionsPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+
+        folderCanvas.SetActive(false); // Animate later
     }
 
     void Update()
