5df070da9cb1b1808580ef327f85d30bece26eda
WCAF0137C <60712316+WCAF0137C@users.noreply.github.com>
2026-01-16 01:33:21 -0500

    Folder blockout wheee

--- Assets/Scripts/Evidence Folder/EvidenceFolder.cs
+++ Assets/Scripts/Evidence Folder/EvidenceFolder.cs
@@ -18,27 +18,32 @@ public class EvidenceFolder : MonoBehaviour
 
     //This script functions as a menu system for our evidence folder
     public GameObject folderCanvas; // Animate later
+    bool menuIsOpen = false;
 
     // Clues section
     public GameObject clueOverviewPanel;
     public GameObject clueDetailsPanel;
-    public Button clueReturnButton;
+    //public Button clueReturnButton;
 
-    public CluesEnum currentCluePanel = CluesEnum.Overview;
+    private CluesEnum currentCluePanel = CluesEnum.Overview;
 
     // Info section
     public GameObject reportPanel;
     public GameObject questionsPanel;
     public GameObject inventoryPanel;
 
-    public Button reportButton;
-    public Button questionsButton;
-    public Button inventoryButton;
+    //public Button reportButton;
+    //public Button questionsButton;
+    //public Button inventoryButton;
 
-    public InfoEnum currentInfoPanel = InfoEnum.Report;
+    private InfoEnum currentInfoPanel = InfoEnum.Report;
 
     void Start()
     {
+        clueOverviewPanel.SetActive(true);
+        clueDetailsPanel.SetActive(false);
+
+        reportPanel.SetActive(true);
         questionsPanel.SetActive(false);
         inventoryPanel.SetActive(false);
 
@@ -47,6 +52,79 @@ public class EvidenceFolder : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab)) // This will need to be changed so a button can affect it later if controller support is desired
+        {
+            if (menuIsOpen)
+            {
+                CloseMenu();
+            }
+            else
+            {
+                OpenMenu();
+            }
+        }
+    }
+
+    public void OpenMenu()
+    {
+        menuIsOpen = true;
+        folderCanvas.SetActive(true);
+
+        // Reset folder pages to defaults?
+        ClueOverviewButton();
+        ReportButton();
+
+        // Need to change camera/cursor control value in game manager
+    }
+
+    public void CloseMenu()
+    {
+        menuIsOpen = false;
+        folderCanvas.SetActive(false);
+
+        // Need to change camera/cursor control value in game manager
+    }
+
+    public void ClueOverviewButton()
+    {
+        clueOverviewPanel.SetActive(true);
+        clueDetailsPanel.SetActive(false);
+
+        currentCluePanel = CluesEnum.Overview;
+    }
+
+    public void ClueDetailsButton() // Currently unused, gonna need to be accessed by specific clue buttons. Maybe take info from scriptable objects?
+    {
+        clueOverviewPanel.SetActive(false);
+        clueDetailsPanel.SetActive(true);
+
+        currentCluePanel = CluesEnum.Details;
+    }
+
+    public void ReportButton()
+    {
+        reportPanel.SetActive(true);
+        questionsPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+
+        currentInfoPanel = InfoEnum.Report;
+    }
+
+    public void QuestionsButton()
+    {
+        reportPanel.SetActive(false);
+        questionsPanel.SetActive(true);
+        inventoryPanel.SetActive(false);
+
+        currentInfoPanel = InfoEnum.Questions;
+    }
+
+    public void InventoryButton()
+    {
+        reportPanel.SetActive(false);
+        questionsPanel.SetActive(false);
+        inventoryPanel.SetActive(true);
+
+        currentInfoPanel = InfoEnum.Inventory;
     }
 }
