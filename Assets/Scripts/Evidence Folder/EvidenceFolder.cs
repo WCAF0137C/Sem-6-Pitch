e65b5c69a88068abd819e3d9424322923c4300ed
WCAF0137C <60712316+WCAF0137C@users.noreply.github.com>
2026-01-15 22:10:24 -0500

    Aaaagh

---
+++ Assets/Scripts/Evidence Folder/EvidenceFolder.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EvidenceFolder : MonoBehaviour
+{
+    //This script functions as a menu system for our evidence folder
+
+    public GameObject clueOverviewPanel;
+    public GameObject clueDetailsPanel;
+    public Button clueReturnButton;
+
+    public GameObject reportPanel;
+    public GameObject questionsPanel;
+    public GameObject inventoryPanel;
+
+    public Button reportButton;
+    public Button questionsButton;
+    public Button inventoryButton;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
